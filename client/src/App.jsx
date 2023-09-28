15b8858b814557e2020ac06bd72dcedf6d268e67
pankshiks <shikawebworks@gmail.com>
2023-09-28 12:16:14 +0530

    update code

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import CssBaseline from "@mui/material/CssBaseline";
+import ChatRoom from "./views/pages/ChatRoom";
+
+function App() {
+  return (
+    <div>
+      <CssBaseline />
+      <ChatRoom />
+    </div>
+  );
+}
+
+export default App;
